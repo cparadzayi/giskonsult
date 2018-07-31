ad15c51d0d1b1c1de3abc9a0d73c963c60749b80
cparadzayi <cparadzayi@gmail.com>
2018-07-31 19:06:08 +0200

    Build the website for production

---
+++ dist/precache-manifest.fd1ca9f16788810f447f1c0580639236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "df9e3e19f6a1b5fdcf71",
+    "url": "/js/chunk-vendors.df9e3e19.js"
+  },
+  {
+    "revision": "1145ea4a154d0b6bc574",
+    "url": "/js/app.1145ea4a.js"
+  },
+  {
+    "revision": "5657c7005608e57cb2d80a4a272cf654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241d17e93e8f2ca2ffad62730c89f71e",
+    "url": "/img/giskonsult-logo.241d17e9.png"
+  },
+  {
+    "revision": "1145ea4a154d0b6bc574",
+    "url": "/css/app.ef2b621b.css"
+  }
+];
